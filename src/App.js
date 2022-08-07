786c8e24a259217c064b0a0d15be0dcd97de8265
sergiohbk <sergiohbk92@gmail.com>
2022-08-07 19:13:36 +0200

    texto y fuente header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const Header = () => (
+  <header className="header-app">
+    <span>Sergio Portero Garcia</span>
+  </header>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
     </div>
   );
 }
